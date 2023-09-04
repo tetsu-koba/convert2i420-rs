35bb219d70872a3fadf874442a1746154f83c3e1
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2023-09-04 14:15:08 +0900

    Add pipe.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::env;
 use std::fs::File;
 use std::io::{Read, Write};
+use std::os::fd::AsRawFd;
 use std::process;
 mod from_i422;
 mod from_nv12;
 mod from_yuyv;
+mod pipe;
 
 type ConvertFunc = fn(&[u8], &mut [u8], u32, u32);
 
@@ -25,6 +27,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let height: u32 = args[4].parse()?;
     let pixel_format = &args[5].to_uppercase();
 
+    let is_pipe_output = if cfg!(linux) && pipe::is_pipe(outfile.as_raw_fd()) {
+        pipe::set_pipe_max_size(outfile.as_raw_fd())?;
+        true
+    } else {
+        false
+    };
     let mut output_data = vec![0u8; (width * height * 3 / 2) as usize];
     let f: ConvertFunc;
     let mut input_data: Vec<u8>;
@@ -53,7 +61,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         f(&input_data, &mut output_data, width, height);
 
         // Write to the output file
-        if let Err(e) = outfile.write_all(&output_data) {
+        if let Err(e) = if is_pipe_output {
+            pipe::vmsplice_single_buffer(&output_data, outfile.as_raw_fd())
+        } else {
+            outfile.write_all(&output_data)
+        } {
             if e.kind() == std::io::ErrorKind::BrokenPipe {
                 break;
             }

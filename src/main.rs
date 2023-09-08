4fc449f18f78e31a36e2f13e0e430eeb1705d973
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2023-09-08 17:08:24 +0900

    Split convert_to_i420().

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,13 @@ mod pipe;
 
 type ConvertFunc = fn(&[u8], &mut [u8], u32, u32);
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() < 6 {
-        println!(
-            "Usage: {} input_file output_file width height pixelformat",
-            args[0]
-        );
-        process::exit(1);
-    }
-
-    let mut infile = File::open(&args[1])?;
-    let mut outfile = File::create(&args[2])?;
-    let width: u32 = args[3].parse()?;
-    let height: u32 = args[4].parse()?;
-    let pixel_format = &args[5].to_uppercase();
-
+fn convert_to_i420(
+    mut infile: File,
+    mut outfile: File,
+    width: u32,
+    height: u32,
+    pixel_format: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
     let is_pipe_output = if cfg!(linux) && pipe::is_pipe(outfile.as_raw_fd()) {
         pipe::set_pipe_max_size(outfile.as_raw_fd())?;
         true
@@ -37,18 +27,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let f: ConvertFunc;
     let mut input_data: Vec<u8>;
 
-    if pixel_format == "YUYV" {
-        input_data = vec![0u8; (width * height * 2) as usize];
-        f = from_yuyv::yuyv_to_i420;
-    } else if pixel_format == "NV12" {
-        input_data = vec![0u8; (width * height * 3 / 2) as usize];
-        f = from_nv12::nv12_to_i420;
-    } else if pixel_format == "YUV422" || pixel_format == "I422" {
-        input_data = vec![0u8; (width * height * 2) as usize];
-        f = from_i422::i422_to_i420;
-    } else {
-        eprintln!("Doesn't support {}", pixel_format);
-        process::exit(1);
+    match pixel_format {
+        "YUYV" => {
+            input_data = vec![0u8; (width * height * 2) as usize];
+            f = from_yuyv::yuyv_to_i420;
+        }
+        "NV12" => {
+            input_data = vec![0u8; (width * height * 3 / 2) as usize];
+            f = from_nv12::nv12_to_i420;
+        }
+        "YUV422" | "I422" => {
+            input_data = vec![0u8; (width * height * 2) as usize];
+            f = from_i422::i422_to_i420;
+        }
+        _ => {
+            return Err(format!("Doesn't support {}", pixel_format).into());
+        }
     }
 
     loop {
@@ -72,6 +66,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             return Err(e.into());
         }
     }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 6 {
+        println!(
+            "Usage: {} input_file output_file width height pixelformat",
+            args[0]
+        );
+        process::exit(1);
+    }
+
+    let infile = File::open(&args[1])?;
+    let outfile = File::create(&args[2])?;
+    let width: u32 = args[3].parse()?;
+    let height: u32 = args[4].parse()?;
+    let pixel_format = &args[5].to_uppercase();
+
+    match pixel_format as &str {
+        "YUYV" | "NV12" | "YUV422" | "I422" => {}
+        _ => {
+            eprintln!("Doesn't support {}", pixel_format);
+            process::exit(1);
+        }
+    }
+
+    convert_to_i420(infile, outfile, width, height, pixel_format)?;
 
     Ok(())
 }
